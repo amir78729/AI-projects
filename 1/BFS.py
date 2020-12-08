db0c3645e5a68f781c0d8049c5e41fd660843720
amir78729 <amir78729@gmail.com>
2020-12-08 04:01:42 +0330

    get inputs completed

--- 1/BFS.py
+++ 1/BFS.py
@@ -1,22 +1,44 @@
-# Python3 Program to print BFS traversal
-# from a given source vertex. BFS(int s)
-# traverses vertices reachable from s.
-from collections import defaultdict
-
-
-# This class represents a directed graph
-# using adjacency list representation
 class Card:
 
-	def __init__(self, color, number):
-		self.color = color
-		self.number = number
+    def __init__(self, number, color):
+        self.color = color
+        self.number = number
+
+
+def split_card(string):
+    color = ""
+    number = ""
+    for i in range(len(string)):
+        if string[i].isdigit():
+            number = number + string[i]
+        elif (('A' <= string[i] <= 'Z') or
+              ('a' <= string[i] <= 'z')):
+            color += string[i]
+    # print(color)
+    # print(number)
+    return number, color
 
 
 if __name__ == '__main__':
-	# k = number of decks
-	# m = number of colors
-	# n = number of each color of cards (from )
-	k, m, n = input().split()
-
-
+    # k = number of sections
+    # m = number of colors
+    # n = number of each color of cards (from )
+    k, m, n = input().split()
+    k, m, n = int(k), int(m), int(n)
+    # we are going to have K sections
+    sections = []
+    for sec in range(k):
+        section = []
+        cards_in_section = input()
+        if cards_in_section == '#':
+            sections.append(None)
+            continue
+        cards_list = cards_in_section.split()
+        print(cards_list)
+        for card in cards_list:
+            num, clr = split_card(card)
+            card_object = Card(num, clr)
+            section.append(card_object)
+        sections.append(section)
+        # sections.append(userList)
+    print(sections)
