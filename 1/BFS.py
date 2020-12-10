742991b8728f86bd5c46c7ba70a02894bf617f72
amir78729 <amir78729@gmail.com>
2020-12-10 22:55:10 +0330

    move function

--- 1/BFS.py
+++ 1/BFS.py
@@ -41,23 +41,48 @@ def print_cards():
             print("")
 
 
-def move_card(origin, destination):
-    print('----------------------------------------------------------------')
-    print("****  MOVE FROM {} TO {}  ****".format(origin, destination))
+def move_card(origin, destination, print_it):
+    if print_it:
+        print("----------------------------------------------------------------\n****  MOVE FROM {} TO {}  ****".format(origin, destination))
     if not sections[origin]:
-        print("!!! section {} is empty".format(origin))
+        if print_it:
+            print("!!! section {} is empty".format(origin))
         print_cards()
         return
     else:
         if not sections[destination]:
-            print(">>> moving card from section {} to  empty section  {}".format(origin, destination))
+            if print_it:
+                print(">>> moving card from section {} to  empty section  {}".format(origin, destination))
             sections[destination].append(sections[origin].pop())
         elif sections[destination][-1].number >= sections[origin][-1].number:
-            print(">>> moving card from section {} to section  {}".format(origin, destination))
+            if print_it:
+                print(">>> moving card from section {} to section  {}".format(origin, destination))
             sections[destination].append(sections[origin].pop())
         else:
-            print("!!! can not move card from section {} to section  {}".format(origin, destination))
-        print_cards()
+            if print_it:
+                print("!!! can not move card from section {} to section  {}".format(origin, destination))
+        if print_it:
+            print_cards()
+
+
+def move():
+    for step in steps:
+        move_card(step[0], step[1], False)
+
+
+def get_inputs():
+    for sec in range(k):
+        section = []
+        cards_in_section = input()
+        if cards_in_section == '#':
+            sections.append(section)
+            continue
+        cards_list = cards_in_section.split()  # "5g 5r 4y" => ["5g", "5r", "4y"]
+        for card in cards_list:
+            num, clr = split_card(card)  # "5g" => "5", "g"
+            card_object = Card(num, clr)  # "5", "g" => Card object
+            section.append(card_object)
+        sections.append(section)
 
 
 if __name__ == '__main__':
@@ -68,29 +93,28 @@ if __name__ == '__main__':
     k, m, n = int(k), int(m), int(n)
     sections = []
     # we are going to have K sections
-    for sec in range(k):
-        section = []
-        cards_in_section = input()
-        if cards_in_section == '#':
-            # sections.append('')
-            # sections[sec] = []
-            sections.append(section)
-            continue
-        cards_list = cards_in_section.split()  # "5g 5r 4y" => ["5g", "5r", "4y"]
-        for card in cards_list:
-            num, clr = split_card(card)  # "5g" => "5", "g"
-            card_object = Card(num, clr)  # "5", "g" => Card object
-            section.append(card_object)
-        sections.append(section)
+    get_inputs()
 
     print_cards()
 
+    steps = []
+    steps.append((3, 4))
+    steps.append((1, 4))
+    steps.append((3, 2))
+    steps.append((2, 4))
+    steps.append((2, 1))
+
+    # print(steps)
+    # steps.append((3, 4))
     # time.sleep(.5)
-    move_card(3, 4)
-    move_card(1, 4)
-    move_card(3, 2)
-    move_card(2, 4)
-    move_card(2, 1)
+    # move_card(3, 4)
+    # move_card(1, 4)
+    # move_card(3, 2)
+    # move_card(2, 4)
+    # move_card(2, 1)
+
+    move()
+    print_cards()
 
     # time.sleep(.5)
 
