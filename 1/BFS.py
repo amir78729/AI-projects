23920b2582e6bd4fec330e9ca0f1ebcdffb1da6a
amir78729 <amir78729@gmail.com>
2020-12-13 23:57:17 +0330

    using deepcopy() for move function

--- 1/BFS.py
+++ 1/BFS.py
@@ -1,4 +1,5 @@
 import time
+import copy
 
 '''
 5 3 5
@@ -23,12 +24,17 @@ class Card:
 
 
 class State:
-    def __init__(self, all_cards, depth, parent, move_from, move_to):
-        self.all_cards = all_cards
+    def __init__(self, cards_sections, depth, parent, move_from, move_to):
+        self.cards_sections = cards_sections
         self.depth = depth
         self.parent = parent
         self.move_from = move_from
         self.move_to = move_to
+
+    # def expand_children(self):
+    #     children = []
+    #     for i in range(len(self.cards_sections)):
+    #         for j in range(len(self.cards_sections)):
 
 
 def split_card(string):
@@ -45,7 +51,7 @@ def split_card(string):
     return number, color
 
 
-def print_cards():
+def print_cards(sections):
     i = 0
     for section in sections:
         print(i, end=" : ")
@@ -59,15 +65,16 @@ def print_cards():
             print("")
 
 
-def move_card(origin, destination, print_it):
+def move_card(sec, origin, destination, print_it):
+    sections = copy.deepcopy(sec)
     if print_it:
         print("----------------------------------------------------------------")
         print("****  MOVE FROM {} TO {}  ****".format(origin, destination))
     if not sections[origin]:
         if print_it:
             print("!!! section {} is empty".format(origin))
-        print_cards()
-        return
+        print_cards(sections)
+        return None
     else:
         if not sections[destination]:
             if print_it:
@@ -80,17 +87,19 @@ def move_card(origin, destination, print_it):
         else:
             if print_it:
                 print("!!! can not move card from section {} to section  {}".format(origin, destination))
+            return None
         if print_it:
-            print_cards()
+            print_cards(sections)
+    return sections
 
 
-def move():
-    for step in steps:
-        move_card(step[0], step[1], True)
-        time.sleep(.2)
+# def move():
+#     for step in steps:
+#         move_card(step[0], step[1], True)
+#         time.sleep(.2)
 
 
-def get_inputs():
+def get_inputs(sections):
     for sec in range(k):
         section = []
         cards_in_section = input()
@@ -103,6 +112,7 @@ def get_inputs():
             card_object = Card(num, clr)  # "5", "g" => Card object
             section.append(card_object)
         sections.append(section)
+    return sections
 
 
 if __name__ == '__main__':
@@ -111,11 +121,12 @@ if __name__ == '__main__':
     # n = number of each color of cards (from )
     k, m, n = input().split()
     k, m, n = int(k), int(m), int(n)
-    sections = []
+    card_sections = []
+    # print_cards(card_sections)
     # we are going to have K sections
-    get_inputs()
+    card_sections = get_inputs(card_sections)
 
-    print_cards()
+    print_cards(card_sections)
 
     steps = []
     steps.append((3, 4))
@@ -127,13 +138,17 @@ if __name__ == '__main__':
     # print(steps)
     # steps.append((3, 4))
     # time.sleep(.5)
-    # move_card(3, 4)
+    print_cards(card_sections)
+    a = move_card(card_sections, 3, 4, False)
+    print_cards(a)
+    print()
+    print_cards(card_sections)
     # move_card(1, 4)
     # move_card(3, 2)
     # move_card(2, 4)
     # move_card(2, 1)
 
-    move()
+    # move()
     # print_cards()
 
     # time.sleep(.5)
