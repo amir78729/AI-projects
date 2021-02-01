6f690c715e4239ed0840f2803e8d75088c668c27
amir78729 <amir78729@gmail.com>
2021-02-01 12:35:04 +0330

    solve by numbers added (not working)

--- 2/Sudoku.py
+++ 2/Sudoku.py
@@ -9,6 +9,12 @@ class Cell:
     def get_num(self):
         return self.num
 
+    def set_col(self, col):
+        self.col = col
+
+    def set_num(self, num):
+        self.num = num
+
     def print_cell(self):
         print("{}{}\t".format(self.num, self.col), end='')
 
@@ -16,6 +22,79 @@ class Cell:
 class State:
     def __init__(self, table):
         self.table = table
+
+    # Returns a boolean which indicates
+    # whether any assigned entry
+    # in the specified row matches
+    # the given number.
+    def used_in_row(self, row, num):
+        for i in range(n):
+            if self.table[row][i].get_num() == num:
+                return True
+        return False
+
+    # Returns a boolean which indicates
+    # whether any assigned entry
+    # in the specified column matches
+    # the given number.
+    def used_in_col(self, col, num):
+        for i in range(n):
+            if self.table[i][col].get_num() == num:
+                return True
+        return False
+
+    def check_location_is_safe(self, row, col, num):
+
+        # Check if 'num' is not already
+        # placed in current row,
+        # current column and current 3x3 box
+        return not self.used_in_row(row, num) and not self.used_in_col(col, num)
+
+    def find_empty_location(self, l):
+        for row in range(n):
+            for col in range(n):
+                if self.table[row][col].get_num() == '*':
+                    l[0] = row
+                    l[1] = col
+                    return True
+        return False
+
+    def solve_sudoku(self):
+
+        # 'l' is a list variable that keeps the
+        # record of row and col in
+        # find_empty_location Function
+        l = [0, 0]
+
+        # If there is no unassigned
+        # location, we are done
+        if not self.find_empty_location(l):
+            return True
+
+        # Assigning list values to row and col
+        # that we got from the above Function
+        row = l[0]
+        col = l[1]
+
+        # consider digits 1 to n
+        for num in range(1, n + 1):
+
+            # if looks promising
+            if self.check_location_is_safe(row, col, num):
+
+                # make tentative assignment
+                self.table[row][col].set_num(num)
+
+                # return, if success,
+                # ya !
+                if self.solve_sudoku:
+                    return True
+
+                # failure, unmake & try again
+                self.table[row][col].set_num('*')
+
+        # this triggers backtracking
+        return False
 
     def is_valid_by_number(self):
         for i in range(n):  # Elements on the main diameter
@@ -75,6 +154,8 @@ if __name__ == '__main__':
         table.append(row)
 
     initial_state = State(table)
+    initial_state.print_table()
+    print(initial_state.solve_sudoku())
     initial_state.print_table()
     print(initial_state.is_valid_by_number())
 
