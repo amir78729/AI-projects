7b6f234ae298f9b0479bc22e9a778fa1af8a0e7f
amir78729 <amir78729@gmail.com>
2021-02-04 11:27:35 +0330

    word counts added

--- 3/guess_the_poet.py
+++ 3/guess_the_poet.py
@@ -1,4 +1,3 @@
-import nltk, re, pprint, string
 from nltk import ngrams, FreqDist
 
 
@@ -10,26 +9,32 @@ def create_dictionary_bigram(lst):
             dictionary[text] = item[1]
     return dictionary
 
+
 def create_dictionary_unigram(lst):
+    number_of_words = 0
     dictionary = {}
     for item in lst:
         if item[1] > 2:
             text = '' + item[0][0]
             dictionary[text] = item[1]
+            number_of_words += item[1]
+    word_count.append(number_of_words)
     return dictionary
 
 
+# P(Ci) = count(Ci) / M
 def p_unigram(ci, poet_index):
     try:
-        return dictionary_unigram[poet_index][ci] / number_of_unigrams[poet_index]
+        return dictionary_unigram[poet_index][ci] / word_count[poet_index]
     except KeyError:
         return 0
 
 
+# P(Ci|Ci-1) = count(Ci-1 Ci) / count(Ci-1)
 def p_bigram(ci, ci_1, poet_index):
     try:
         string = '' + ci_1 + ' ' + ci
-        return dictionary_bigram[poet_index][string] / p_unigram(ci_1, poet_index)
+        return dictionary_bigram[poet_index][string] / dictionary_unigram[poet_index][ci_1]
     except KeyError:
         return 0
 
@@ -44,7 +49,7 @@ if __name__ == '__main__':
     λ3 = '?'
     ε = '?'
     poets = ['ferdowsi', 'hafez', 'molavi']
-    dictionary_unigram, dictionary_bigram, number_of_unigrams, number_of_bigrams = [], [], [], []
+    dictionary_unigram, dictionary_bigram, number_of_unigrams, number_of_bigrams, word_count = [], [], [], [], []
     index = 0
     for poet in poets:
         print('>>> CREATING LANGUAGE MODELS FOR \"{}\"...'.format(poet.upper()))
@@ -63,17 +68,25 @@ if __name__ == '__main__':
         number_of_bigrams.append(all_counts[2].B())
         dictionary_unigram.append(all_counts[1].most_common(number_of_unigrams[index]))
         dictionary_unigram[index] = create_dictionary_unigram(dictionary_unigram[index])
-        print('\t>>> UNIGRAM HAS BEEN CREATED FOR \"{}\" WITH {} ELEMENTS'.format(poet.upper(), number_of_unigrams[index]))
+        print('\t>>> UNIGRAM: {} ELEMENTS'.format(number_of_unigrams[index]))
         dictionary_bigram.append(all_counts[2].most_common(number_of_bigrams[index]))
         dictionary_bigram[index] = create_dictionary_bigram(dictionary_bigram[index])
-        print('\t>>> BIGRAM HAS BEEN CREATED FOR \"{}\" WITH {} ELEMENTS'.format(poet.upper(), number_of_bigrams[index]))
+        print('\t>>> BIGRAM: {} ELEMENTS'.format(number_of_bigrams[index]))
+        print('\t>>> NUMBER OF WORDS: {}'.format(word_count[index]))
         index += 1
         print()
     print(' - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n')
 
-    # print(p_unigram('اسب', 0))
-    # ci_1 = 'عشق'
-    # ci = 'را'
-    # print(p_bigram(ci, ci_1, 2))
+    print(p_unigram('اسب', 0))
+    ci_1 = 'عشق'
+    ci = 'را'
+    s = 'عشق را'
+    s = s.split(' ')
+    print(s)
+    ci_1 = s[0]
+    ci = s[1]
+    print(ci_1)
+    print(ci)
+    print(p_bigram(ci, ci_1, 2))
 
 
